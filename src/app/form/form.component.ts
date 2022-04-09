af20c85490559557de78938d4d3ae98faa69578d
smetto20 <sandra.metto@student.moringaschool.com>
2022-04-09 15:38:54 +0300

    ft adds a way of  getingt user form inputs

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
+import { DisplayComponent } from '../display/display.component';
+import { Quote } from './quote';
+
 
 @Component({
   selector: 'app-form',
@@ -6,8 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
-
-  constructor() { }
+quote:Quote = new Quote("enter quote","author","person")
+// quote = {
+//   quote:"i am great"
+  
+// }
+log(){
+  // console.log(this.quote.quote,this.quote.author,this.quote.person)
+  document.write("app-details")
+}
+  constructor() {}
 
   ngOnInit(): void {
   }
--- src/app/form/quote.ts
+++ src/app/form/quote.ts
@@ -1,2 +1,11 @@
 export class Quote {
+    quote:string
+    author:string
+    person:string
+
+    constructor(quote:string,author :string,person:string){
+        this.quote = quote
+        this.author= author
+        this.person= person
+    }
 }
